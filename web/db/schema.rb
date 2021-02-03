2d6f6ada8b5a392d8c8a61b7327b99232628041c
ichigo-dev <ichigo@web-freework.com>
2021-02-03 18:58:21 +0900

    migrate words

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_030206) do
+ActiveRecord::Schema.define(version: 2021_02_03_094939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,14 @@ ActiveRecord::Schema.define(version: 2021_02_03_030206) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  create_table "words", force: :cascade do |t|
+    t.string "origin", null: false
+    t.string "text", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_words_on_user_id"
+  end
+
+  add_foreign_key "words", "users"
 end
