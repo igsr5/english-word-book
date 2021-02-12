c046f4427b404c5a825488dfd6d26d414e4f6ed8
ichigo-dev <ichigo@web-freework.com>
2021-02-12 13:19:05 +0900

    add route users

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "translates#index"
+  namespace 'api' do
+    namespace 'public' do
+      resources :users
+    end
+  end
 end
