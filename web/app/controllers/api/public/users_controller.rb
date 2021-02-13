4403f4df5108d8c60685a9c9f40f28c96013d248
ichigo-dev <ichigo@web-freework.com>
2021-02-13 16:10:45 +0900

    fix route

--- web/app/controllers/api/public/users_controller.rb
+++ web/app/controllers/api/public/users_controller.rb
@@ -1,46 +1,44 @@
 module Api
-  module Public
-    class UsersController < ApplicationController
-      def index
-        users = User.all
-        render json: { data: users, message: 'SUCCESS'}
+  class UsersController < ApplicationController
+    def index
+      users = User.all
+      render json: { data: users, message: 'SUCCESS'}
+    end
+
+    def show
+      user = User.find(params[:id])
+      render json: { data: user, message: 'SUCCESS'}
+    end
+
+    def create
+      user = User.new(user_param)
+      if user.save
+        render json: { data: user, message: 'SUCCESS' }
+      else
+        render json: { message: user.errors }
       end
+    end
 
-      def show
-        user = User.find(params[:id])
-        render json: { data: user, message: 'SUCCESS'}
+    def update
+      user = User.find(params[:id])
+      if user.update(user_param)
+        render json: { data: user, message: 'SUCCESS' }
+      else
+        render json: { message: user.errors }
+
       end
+    end
 
-      def create
-        user = User.new(user_param)
-        if user.save
-          render json: { data: user, message: 'SUCCESS' }
-        else
-          render json: { message: user.errors }
-        end
-      end
+    def destroy
+      user = User.find(params[:id])
+      user.destroy
+      render json: { data: user, message: 'SUCCESS'}
+    end
 
-      def update
-        user = User.find(params[:id])
-        if user.update(user_param)
-          render json: { data: user, message: 'SUCCESS' }
-        else
-          render json: { message: user.errors }
+    private
 
-        end
-      end
-
-      def destroy
-        user = User.find(params[:id])
-        user.destroy
-        render json: { data: user, message: 'SUCCESS'}
-      end
-
-      private
-
-      def user_param
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      end
+    def user_param
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
   end
 end
