24462e28ed14c82e2b5704177c423a0083f87af0
ichigo-dev <ichigo@web-freework.com>
2021-02-16 12:13:08 +0900

    add action update in auth_controller

--- web/app/controllers/auth_controller.rb
+++ web/app/controllers/auth_controller.rb
@@ -1,2 +1,12 @@
 class AuthController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    token = ''
+    status = 'unauthoraized'
+    if user && user.authenticate(params[:password])
+      token = Session.create(user)
+      status = 'created'
+    end
+    render json: { token: token, status: status }
+  end
 end
