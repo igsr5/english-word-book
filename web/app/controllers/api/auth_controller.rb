14bb17e5afc2086242387df1da7681be2d4f2bc0
ichigo-dev <ichigo@web-freework.com>
2021-02-16 23:31:23 +0900

    update auth_controller#create

--- web/app/controllers/api/auth_controller.rb
+++ web/app/controllers/api/auth_controller.rb
@@ -1,12 +1,14 @@
-class AuthController < ApplicationController
-  def create
-    user = User.find_by(email: params[:email])
-    token = ''
-    status = 'unauthoraized'
-    if user && user.authenticate(params[:password])
-      token = Session.create(user)
-      status = 'created'
+module Api
+  class AuthController < ApplicationController
+    def create
+      user = User.find_by(email: params[:email])
+      token = ''
+      status = user ? 'unauthoraized' : 'undefined'
+      if user && user.authenticate(params[:password])
+        token = Session.create(user)
+        status = 'created'
+      end
+      render json: { token: token, status: status }
     end
-    render json: { token: token, status: status }
   end
 end
