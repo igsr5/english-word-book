a9b06b0512abcde65b2616659c8a9cd540aa9adf
ichigo-dev <ichigo@web-freework.com>
2021-02-16 12:08:28 +0900

    update concerns/session.rb

---
+++ web/app/controllers/concerns/session.rb
@@ -0,0 +1,16 @@
+module Session
+  def self.create(user)
+    token = SecureRandom.hex(64)
+    REDIS.mapped_hmset(
+      token,
+      'user_id' => user.id,
+      'role' => user.role,
+    )
+    REDIS.expire(token, Settings.redis.timeout)
+    return token
+  end
+
+  def self.get(token)
+    REDIS.hgetall(token)
+  end
+end
