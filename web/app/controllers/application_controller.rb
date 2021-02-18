878e1eabc8e0c3d656d92be76e65d41ab796c07b
ichigo-dev <ichigo@web-freework.com>
2021-02-18 20:05:51 +0900

    fix bug application_controller

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -3,17 +3,17 @@ class ApplicationController < ActionController::API
   before_action :require_login
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
-  @@session = {}
+  @session = {}
 
   def require_login
-    render json: { error: 'unauthorized token' , status: 'unauthorized token' } if @@session.blank?
+    render json: { error: 'unauthorized token' , status: 'unauthorized token' } if @session.blank?
   end
 
   private
 
   def set_session
     authenticate_with_http_token do |token, options|
-      @@session = Session.get(token)
+      @session = Session.get(token)
     end
   end
 end
