4e1d7e17029f8d1db17606b21f4076d2fcc5426a
ichigo-dev <ichigo@web-freework.com>
2021-02-20 22:59:47 +0900

    add action create in translate_controller

--- web/app/controllers/api/translate_controller.rb
+++ web/app/controllers/api/translate_controller.rb
@@ -1,2 +1,11 @@
-class TransrateController < ApplicationController
+module Api
+  class TranslateController < ApplicationController
+    skip_before_action :require_login, only: [:create]
+    def create
+      test = 'Hello, world!'
+      target = 'ja'
+      translate = TranslateService.translate(test, target)
+      render json: { text: translate.text, origin: translate.origin, to: translate.to, from: translate.from }
+    end
+  end
 end
