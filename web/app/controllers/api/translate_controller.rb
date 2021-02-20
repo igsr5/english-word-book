43b37b9746d946f620311e2a53f33a98f0b46310
ichigo-dev <ichigo@web-freework.com>
2021-02-20 23:03:56 +0900

    update action create in translate_controller

--- web/app/controllers/api/translate_controller.rb
+++ web/app/controllers/api/translate_controller.rb
@@ -2,10 +2,10 @@ module Api
   class TranslateController < ApplicationController
     skip_before_action :require_login, only: [:create]
     def create
-      test = 'Hello, world!'
-      target = 'ja'
-      translate = TranslateService.translate(test, target)
-      render json: { text: translate.text, origin: translate.origin, to: translate.to, from: translate.from }
+      status = 'failed'
+      translate = TranslateService.translate(params[:text], params[:target])
+      status = 'success' if translate.text.present? && translate.origin.present?
+      render json: { status: status, text: translate.text, origin: translate.origin, to: translate.to, from: translate.from }
     end
   end
 end
