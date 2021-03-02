402f64e97cd036285970039f6389e5546a3ffe2c
ichigo-dev <ichigo@web-freework.com>
2021-03-02 15:36:09 +0900

    update transrate_request_spec

--- web/spec/requests/transrate_request_spec.rb
+++ web/spec/requests/transrate_request_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Transrates", type: :request do
+  let(:json) { JSON.parse(response.body) }
   describe 'POST: api/translate' do
     before do
       @params = {
@@ -12,6 +13,11 @@ RSpec.describe "Transrates", type: :request do
 
     it 'status is 200' do
       expect(response.status).to eq(200)
+    end
+
+    it 'can translate the word' do
+      expect(json['text']).to eq('こんにちは')
+      expect(json['origin']).to eq('hello')
     end
   end
 end
