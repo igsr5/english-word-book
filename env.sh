44f75375f1edee5c326099d6bfffb1923ed2c344
ichigo-dev <ichigo@web-freework.com>
2021-04-08 00:18:36 +0900

    fix bundle mimemagic

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 # ==============================================================================
 # env
 # ==============================================================================
-project_name='raills/vue'
+project_name='english-word-book'
 alias docker-compose="docker-compose -p $project_name"
 alias build="docker-compose build"
 alias up="docker-compose up"
@@ -14,5 +14,5 @@ alias seed_fu="rake db:seed_fu FIXTURE_PATH=db/seeds/development/"
 alias rspec="docker-compose run -e RAILS_ENV=test --rm web bundle exec rspec"
 alias rubocop="docker-compose run -e RAILS_ENV=development --rm web bundle exec rubocop -DESP"
 alias rubocopa="docker-compose run -e RAILS_ENV=development --rm web bundle exec rubocop -a"
-alias yarn="docker-compose run -e RAILS_ENV=development --rm app yarn"
-alias npm="docker-compose run -e RAILS_ENV=development --rm app npm"
+alias yarn="docker-compose run --rm app yarn"
+alias npm="docker-compose run --rm app npm"
